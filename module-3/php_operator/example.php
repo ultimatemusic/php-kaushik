0ec3b6ef6c3bf81754c378cb00a88d5a25b0cb94
ultimatemusic <chauhankaushik7102004@gmai.com>
2025-04-29 16:51:09 +0530

    first commit

---
+++ module-3/php_operator/example.php
@@ -0,0 +1,54 @@
+<?php
+// add
+//     $a=10;
+//     $b=20;
+//     echo "add : ".$a+$b;
+
+// Area of a circle
+// $pi=3.14159265359;
+// $r=10;
+// $area=$pi*pow($r,2);
+// echo "area of circle is : ".$area;
+
+
+// area of triangle 
+
+// $b=10;
+// $h=15;
+// $area=1/2*$b*$h;
+// echo $area;
+
+
+// Simple Interest Calculator
+// $p=20000;
+// $r=8.3;
+// $t=5;
+// $si=($p*$r*$t)/100;
+// echo "Interest : ".$si."<br>";
+// echo "total amount : ".$si+$p;
+
+
+//incriment op
+
+// $i=10;
+// $i++;
+// echo "$i";
+
+// $i--;
+// echo "$i";
+
+// $a=10;
+// $b=20;
+// $a+=$b;
+// echo "$a";
+
+
+// $a=10;
+// $b=20;
+// echo "a= $a , b=$b";
+// $a=$a+$b-($b=$a);
+// echo "<br>";
+// echo "after swap :a= $a , b=$b";
+
+
+?>
